7ed428a829023b045dca430be957febb37dc8c43
paulv160 <paul.vilimas@gmail.com>
2020-12-08 18:16:05 -0500

    finished nonrect method

--- randomize.py
+++ randomize.py
@@ -1,19 +1,62 @@
 from item import Item
 from room import Room
 import settings
-from random import shuffle
+from random import shuffle, choice
 from copy import deepcopy
+from math import sqrt, ceil, floor
 
-def randomizeRooms():
+
+def randomizeRoomsNonRect():
     roomList = []
-    possibleRoomNames = ["Library", "Kitchen", "Terrace", "Fountain", "Engine Room", "Garden", "Bedroom", "AP Office", "YSR Headquarters"]
-    shuffledList = deepcopy(possibleRoomNames)
-    shuffle(shuffledList)
-    for i in range(0, len(possibleRoomNames)):
-        roomList.append(Room(shuffledList[i], i, []))
-    roomListCopy = deepcopy(roomList)
-    shuffle(roomListCopy)
-    roomGrid = [roomListCopy[0:3], roomListCopy[3:6], roomListCopy[6:9]]
+    possibleRoomNames = settings.possibleRoomNames
+    shuffledNameList = deepcopy(possibleRoomNames)
+    shuffle(shuffledNameList)
+    # height and width of a square grid
+    gridSize = ceil(sqrt(len(possibleRoomNames))) + 2
+    roomGrid = []
+    roomList = []
+    for i in range(len(possibleRoomNames)):
+        roomList.append(Room(shuffledNameList[i], i, []))
+
+    for i in range(gridSize):
+        roomGrid.append([])
+        for j in range(gridSize):
+            roomGrid[i].append(None)
+
+    openTiles = []  # a list of (row, col) tuples corresponding to grid squares
+    openTiles.append((floor(gridSize/2), floor(gridSize/2)))
+    for room in roomList:
+        currentTile = choice(openTiles)
+        openTiles.pop(openTiles.index(currentTile))
+        row = currentTile[0]
+        col = currentTile[1]
+        roomGrid[row][col] = room
+        for v in settings.borderingTileVectors:
+            rowV = row + v[0]
+            colV = col + v[1]
+            if(rowV < 0 or rowV >= gridSize or colV < 0 or colV >= gridSize):  # establish bounds
+                continue
+            openTiles.append((rowV, colV))
+            openTiles = list(set(openTiles))
+
+    """print("")
+
+    for i in finalList:
+        print(str(i))
+    
+    print("")"""
+
+    startingRoomID = roomGrid[floor(gridSize/2)][floor(gridSize/2)].ID
+    return roomList, roomGrid, startingRoomID
+
+def randomizeRooms(): #old method will not be used
+    roomList = []
+    possibleRoomNames = settings.possibleRoomNames
+    shuffledNameList = deepcopy(possibleRoomNames)
+    shuffle(shuffledNameList)
+    for i in range(len(possibleRoomNames)):
+        roomList.append(Room(shuffledNameList[i], i, []))
+    roomGrid = [roomList[0:3], roomList[3:6], roomList[6:9]]
     # north = row-1, south = row+1, west = col-1, east = col+1
     for row in range(len(roomGrid)):
         for col in range(len(roomGrid[row])):
