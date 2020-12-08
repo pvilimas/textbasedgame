eb3f710a40e2420dcc6f17477879e7dd2ae5352c
paulv160 <paul.vilimas@gmail.com>
2020-12-07 21:39:30 -0500

    updated settings

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
 errorMsg = "Room not found. "
 cannotMoveMsg = "Cannot move that direction. "
 invalidDirMsg = "Not a valid direction. "
+
+possibleRoomNames = ["Library", "Kitchen", "Terrace", "Fountain", "Engine Room", "Garden", "Bedroom", "AP Office", "YSR Headquarters"]
