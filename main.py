9c59c422b5bfbd4c5da5abc239192b01a1f0ec25
paulv160 <paul.vilimas@gmail.com>
2020-12-12 19:50:15 -0500

    input processing done

--- main.py
+++ main.py
@@ -6,47 +6,33 @@ import settings
 roomList = initializeManual()
 
 def showDestinations(room):
-    print('{}:'.format(room.ID))
+    print(f'{room.ID}:')
     for k in room.destinations.keys():
-        print(room.destinations[k])
-
-#grid based test methods, will not be used
-"""def showRooms():
-    s = ""
-    for row in range(len(roomGrid)):
-        for col in range(len(roomGrid[row])):
-            room = roomGrid[row][col]
-            if (room is not None):
-                s += '{:shorter} '.format(room)
-            else:
-                s += 'None'.ljust(18, ' ')
-        s += "\n"
-    print(s)
-
-
-def showRoom(row, col):
-    if(roomGrid[row][col] is not None):
-        print('{:short}'.format(roomGrid[row][col]))
-    else:
-        print('None')"""
+        print(f'{k[0:1]} - {room.destinations[k]}')
 
 currentRoomID = 0
 currentRoom = roomList[currentRoomID]
 
 #later on, change this to GUI/pygame based
 def display(text):
-    print("\n> {}\n".format(text))
+    print(f'\n> {text}\n')
+
+def processInput(i):
+    for inputList in settings.inputModes:
+        if i in inputList:
+            return inputList[0]
+    else:
+        return i
 
 def move(dir):
     global roomList, currentRoom, currentRoomID, movedThisTurn
     if(currentRoom.destinations[dir] is not None):
         currentRoomID = currentRoom.destinations[dir]
         currentRoom = roomList[currentRoomID]
-        display("You went " + str(dir) + ". ")
+        display(f'You went {str(dir)}. ')
         movedThisTurn = True
     else:
-        display("You went " + str(dir) + ". " +
-                settings.errorMsg + currentRoom.msgOnStay)
+        display(f'You went {str(dir)}. {settings.errorMsg + currentRoom.msgOnStay}')
         movedThisTurn = False
 
 # ------- MAIN GAME LOOP ------- #
@@ -57,13 +43,13 @@ while not crashed:
     currentRoom = roomList[currentRoomID]
     showDestinations(currentRoom)
     try:
-        newDir = input("> {}\n".format(currentRoom.msgOnEnter)) if (
-            movedThisTurn and currentRoom is not None) else input("> ")
+        newDir = processInput(input(f'> {currentRoom.msgOnEnter}\n')) if (
+            movedThisTurn and currentRoom is not None) else input('> ')
     except AttributeError:
         newDir = None
     movedThisTurn = False
     try:
-        move(newDir.replace("\n", ""))
+        move(newDir.replace('\n', ''))
     except KeyError:
         display(settings.invalidDirMsg)
     except AttributeError:
